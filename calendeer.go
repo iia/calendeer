9297e6be942799e1608e4c7af69360ec64d77fb6
Ishraq Ibne Ashraf <ishraq.i.ashraf@gmail.com>
2021-02-28 12:27:16 +0100

    Implementation of trash collection iCalendar download from abfall.io

--- calendeer.go
+++ calendeer.go
@@ -1,29 +1,79 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	"github.com/gin-gonic/gin"
+    "fmt"
+    "net/http"
+    "github.com/gin-gonic/gin"
 )
 
-func get() {
-	tckr := time.NewTicker(4 * time.Second)
+func getTrashCalendar(ctxGin *gin.Context) {
+    client := &http.Client{}
+    req, err := http.NewRequest(
+        "GET",
+        "https://api.abfall.io/",
+        nil,
+    )
 
-	for {
-		select {
-			case <-tckr.C:
-				fmt.Println("GET")
-		}
-	}
+    if err != nil {
+        fmt.Println(err)
+
+        ctxGin.Status(http.StatusServiceUnavailable)
+
+        return
+    }
+
+    req.Header.Set(
+        "User-Agent",
+        "Mozilla/5.0 (X11; Linux x86_64) " +
+        "AppleWebKit/537.36 (KHTML, like Gecko) " +
+        "Chrome/88.0.4324.182 " +
+        "Safari/537.36",
+    )
+
+    queryURL := req.URL.Query()
+
+    queryURL.Add("t", "ics")
+    queryURL.Add("s", "57a5732bbba87512418093fdde1497df")
+    queryURL.Add("kh", "DaA02103019b46345f1998698563DaAd")
+
+    req.URL.RawQuery = queryURL.Encode()
+
+    res, err := client.Do(req)
+
+    if err != nil {
+        fmt.Println(err)
+
+        ctxGin.Status(http.StatusServiceUnavailable)
+
+        return
+    }
+
+    reader := res.Body
+
+    defer reader.Close()
+
+    extraHeaders := map[string]string {
+        "Content-Disposition": `attachment; filename="trash_calendar.ics"`,
+    }
+
+    ctxGin.DataFromReader(
+        http.StatusOK,
+        res.ContentLength,
+        res.Header.Get("Content-Type"),
+        reader,
+        extraHeaders,
+    )
 }
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	go get()
-	r.Run(":5000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+    ginSrv := gin.Default()
+
+    ginSrv.GET(
+        "/get/trash_calendar",
+        func (ctxGin *gin.Context) {
+            getTrashCalendar(ctxGin)
+        },
+    )
+
+    ginSrv.Run(":5000")
 }
